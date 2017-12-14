eba8c73e417354ee41cd8eda943a5b9ff6114fc1
DansYU <fly198982@126.com>
2017-12-14 12:57:06 +0800

    Add files via upload

---
+++ iri2016.py
@@ -0,0 +1,33 @@
+#%%
+import requests
+import re
+
+i_url = 'https://omniweb.gsfc.nasa.gov/cgi/vitmo/vitmo_model.cgi'
+url_headers = {
+    'User-Agent':
+    ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+     '(KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36')
+}
+
+iri_post = {'model': 'iri_2016', 'year': '2000', 'month': '1', 'day': '1',
+            'time_flag': '0', 'hour': '1.5', 'geo_flag': '0', 'latitude': '50',
+            'longitude': '40', 'height': '100', 'profile': '1', 'start': '100',
+            'stop': '2000', 'step': '50', 'sun_n': '', 'ion_n': '', 'radio_f': '',
+            'radio_f81': '', 'htec_max': '', 'ne_top': '0', 'imap': '0',
+            'ffof2': '0', 'hhmf2': '0', 'ib0': '2', 'probab': '0', 'fauroralb': '1',
+            'ffoE': '1', 'dreg': '0', 'tset': '0', 'icomp': '0', 'nmf2': '0',
+            'hmf2': '0', 'user_nme': '0', 'user_hme': '0', 'user_B0': '0',
+            'format': '0', 'vars': range(0, 61), 'linestyle': 'solid',
+            'charsize': '', 'symbol': '2', 'symsize': '', 'yscale': 'Linear',
+            'xscale': 'Linear', 'imagex': '640', 'imagey': '480'
+            }
+
+
+response = requests.post(i_url, data=iri_post)
+
+pattern = re.compile('<pre>(.*?)</pre><HR>',
+                     re.S)
+
+results = re.findall(pattern, response.text)
+for result in results:
+    print(result)
